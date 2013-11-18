46d58653539c3cdfc9259cab69f6a6170c501a36
Senthuran Sivasambo <senthuran.sivasambo@ryerson.ca>
2013-11-17 20:54:32 -0500

    First edit

---
+++ Lab 4/src/Lab4.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Lab4.cpp
+// Author      : Senthuran Sivasambo
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+//https://github.com/ssivasambo/Lab-4.git
